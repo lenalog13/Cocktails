5b6c78eb269e670a997fa6bdff4de483f31ac087
lenalog13 <56520937+lenalog13@users.noreply.github.com>
2023-09-11 16:50:56 +0300

    adding UICillectionViewController

---
+++ Cocktails/ViewControllers/MainCollectionViewController.swift
@@ -0,0 +1,118 @@
+//
+//  MainCollectionViewController.swift
+//  Cocktails
+//
+//  Created by Елена Логинова on 11.09.2023.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+final class MainCollectionViewController: UICollectionViewController {
+    
+    
+    private let link = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+    private var cocktailsList: [Cocktail] = []
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        
+        fetchCocktail()
+    }
+
+  
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
+        /*
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let indexPath = collectionView.indexPathsForSelectedItems {
+            guard
+                let detailVS = segue.destination as? DetailViewController else { return }
+            detailVS.cocktail = cocktailsList[indexPath.item]
+        }
+    }
+         */
+    
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        cocktailsList.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+    
+    private func fetchCocktail() {
+        NetworkManager.shared.fetchCocktail(from: link) { [weak self] result in
+            switch result{
+            case .success(let cocktails):
+                self?.cocktailsList = cocktails
+                self?.collectionView.reloadData()
+            case .failure(let error):
+                self?.showAlert(title: "Error", message: error.localizedDescription)
+            }
+        }
+    }
+    
+    
+    private func showAlert(title: String, message: String) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title,
+                                          message: message,
+                                          preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default)
+            alert.addAction(okAction)
+            self.present(alert, animated: true)
+        }
+    }
+
+}
